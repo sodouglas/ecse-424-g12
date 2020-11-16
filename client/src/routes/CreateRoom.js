69d07f71c112553275e829ff5a516212d6f63ff9
Noah-C-Z <nczwack@gmail.com>
2020-11-15 22:56:14 -0500

    Add ability to join exisiting room with ID

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -2,13 +2,35 @@ import React from "react";
 import { v1 as uuid } from "uuid";
 
 const CreateRoom = (props) => {
+    let roomId;
+
     function create() {
         const id = uuid();
         props.history.push(`/room/${id}`);
     }
 
+    function goToRoom() {
+        props.history.push(`/room/${roomId}`);
+    }
+
+    function updateRoom(target) {
+        roomId = document.getElementById("room-input").value;
+        console.log(roomId);
+    }
+
     return (
-        <button onClick={create}>Create room</button>
+        <div>
+            <h1>Welcome to SPACE Video Conferencing!</h1>
+            <input
+                id="room-input"
+                onChange={updateRoom}
+                placeholder="Room ID"
+                ></input>
+            <button onClick={goToRoom}>Join room</button>
+            <br/>
+            <p>-------or-------</p>
+            <button onClick={create}>Create room</button>
+        </div>
     );
 };
 
